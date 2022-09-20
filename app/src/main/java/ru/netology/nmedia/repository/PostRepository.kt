81cccd95fed713bd3f6e5a3bef2fa99b5fa480d4
Марина Жаринова <phyrebirrd@gmail.com>
2022-09-20 18:38:23 +0500

    recycler view implementation added

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    fun get(): LiveData<Post>
-    fun like()
-    fun addShareByClick()
-    fun addImpressionByClick()
+    fun getAll(): LiveData<List<Post>>
+    fun likeById(id: Long)
+    fun addLikeToLiked(post: Post): Int
+    fun addShareByClick(id: Long)
+    fun addImpressionByClick(id: Long)
 }
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -7,8 +7,8 @@ import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 class PostViewModel : ViewModel(){
     // упрощённый вариант
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
-    val data = repository.get()
-    fun like() = repository.like()
-    fun addShareByClick() = repository.addShareByClick()
-    fun addImpressionByClick() = repository.addImpressionByClick()
+    val data = repository.getAll()
+    fun likeById(id: Long) = repository.likeById(id)
+    fun addShareByClick(id: Long) = repository.addShareByClick(id)
+    fun addImpressionByClick(id: Long) = repository.addImpressionByClick(id)
 }
